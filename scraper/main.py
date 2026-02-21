da3ab31c582cdd44a0efed949adaf127a3f47339
matthewmonn-gh <matthewm002@gmail.com>
2026-02-21 11:10:26 -0800

    1. Changed 'game_list.txt' to 'my_game_list.txt' 2. Added wifes game list as a separate file. 3. Made more progress in 'main.py' file to loop through the text files

--- scraper/main.py
+++ scraper/main.py
@@ -1,20 +1,26 @@
 from video_games import VideoGame
 import pandas as pd
+import time
+import random
 
 
 def main():
-    link_to_the_past = VideoGame('The Legend of Zelda: A Link To The Past')
-    create_data_frame(link_to_the_past)
+    my_game_list_as_objects = create_all_video_game_objects(
+        'docs/my_game_list.txt')
+    wifes_game_list_as_objects = create_all_video_game_objects(
+        'docs/wifes_game_list.txt')
+    # link_to_the_past = VideoGame('The Legend of Zelda: A Link To The Past')
+    # create_data_frame(link_to_the_past)
     # print(vars(link_to_the_past))
 
 
-def create_data_frame(video_games: VideoGame):
+def create_data_frame(video_games: list[VideoGame]):
     list_of_video_games = []
     # list_of_video_games.append(video_games.as_dict())
     list_of_video_games.append(video_games)
     print(list_of_video_games)
     print()
-    df = pd.DataFrame([p.as_dict() for p in list_of_video_games])
+    df = pd.DataFrame([game.as_dict() for game in list_of_video_games])
     print(df)
     df.index.name = 'My Game Rankings'
     df.index += 1
@@ -22,8 +28,15 @@ def create_data_frame(video_games: VideoGame):
     pass
 
 
-def create_all_video_game_objects():
-    return 'asd'
+def create_all_video_game_objects(file_path: str) -> list:
+    list_of_video_games = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            line = line.strip()
+            video_game_object = VideoGame(line)
+            time.sleep(random.randint(0, 3))
+            list_of_video_games.append(video_game_object)
+    return list_of_video_games
 
 
 if __name__ == "__main__":
